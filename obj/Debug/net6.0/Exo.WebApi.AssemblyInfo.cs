2963b43ce7f2f918ebd8a6260ceb6e3d2380b4b9
Carla Mariana <carlamab2808@outlook.com>
2024-08-24 17:04:04 -0300

    .

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcb2a8c46cba29ace76c514659ec68f90f7d4e01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46cb87065500826f3760ed42786539c7d3c620a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
